91ab1cfd91aa7128e5f1c5598615b778281168e8
Kyran4 <kyranhannah@gmail.com>
2026-08-31 09:29:04 +1200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CampusPulse
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CampusPulse")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CampusPulse.Pages")]
