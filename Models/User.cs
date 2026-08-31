6fd2bfdc793ed47286731e38f741dd7cc6e1c697
nathanael-u <141211711@techtorium.ac.nz>
2026-09-01 09:00:22 +1200

    added comment, reaction, and report models

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CampusPulse.Models
+﻿namespace CampusPulse.Models
 {
-    internal class User
+    public class User
     {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string UserEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Student";
+
+        public bool isActive { get; set; } = true;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
 }
