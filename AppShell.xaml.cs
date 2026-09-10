9d67c3d10607ca0370e2555c1a3fe2c607002b76
nathanael-u <141211711@techtorium.ac.nz>
2026-09-10 21:12:19 +1200

    fixed shell navigation

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace CampusPulse
+﻿using CampusPulse.Views;
+
+namespace CampusPulse;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+
+        Routing.RegisterRoute(nameof(EventDetailsPage), typeof(EventDetailsPage));
+        Routing.RegisterRoute(nameof(PostDetailsPage), typeof(PostDetailsPage));
+
+        Routing.RegisterRoute(nameof(AdminDashboardPage), typeof(AdminDashboardPage));
+        Routing.RegisterRoute(nameof(AdminModerationPage), typeof(AdminModerationPage));
     }
-}
+}  
