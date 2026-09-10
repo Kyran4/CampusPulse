ab1ccb0d5f470e7acac51f3bf609f940cd91c394
nathanael-u <141211711@techtorium.ac.nz>
2026-09-10 12:02:27 +1200

    feature: added interests api

---
+++ CampusPulse.Api/Controllers/InterestsController.cs
@@ -0,0 +1,27 @@
+﻿using CampusPulse.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusPulse.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InterestsController : ControllerBase
+{
+    private static readonly List<Interest> Interests = new()
+    {
+        new Interest { CategoryId = 1, Name = "Sports" },
+        new Interest { CategoryId = 2, Name = "Technology" },
+        new Interest { CategoryId = 3, Name = "Gaming" },
+        new Interest { CategoryId = 4, Name = "Music" },
+        new Interest { CategoryId = 5, Name = "Cultural Events" },
+        new Interest { CategoryId = 6, Name = "Careers" },
+        new Interest { CategoryId = 7, Name = "Study Support" },
+        new Interest { CategoryId = 8, Name = "Volunteering" }
+    };
+
+    [HttpGet]
+    public ActionResult<List<Interest>> GetInterests()
+    {
+        return Ok(Interests);
+    }
+}
---
+++ CampusPulse.Api/Models/Interest.cs
@@ -0,0 +1,9 @@
+﻿namespace CampusPulse.Api.Models
+{
+    public class Interest
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
